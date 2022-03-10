0925ebd35dee99548130dcf8d482415beeb5086b
VladimirBadzgaradze <vova90688@gmail.com>
2022-03-10 17:47:41 +0300

    Промежуточный этап создания приложения

---
+++ ya_mars_middle_22-master/jobs_api.py
@@ -0,0 +1,24 @@
+import flask
+from flask import jsonify
+
+from data import db_session
+from data.jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'news_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_news():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('job', 'work_size', 'colloborators'))
+                 for item in jobs]
+        }
+    )
